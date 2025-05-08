e950cb892dbbb125bd058c3cfe2a95d7624bcdf9
Aidan Shotland <aidan.shotland@gmail.com>
2025-05-08 16:36:15 -0700

    added a speedy ghost

--- Assets/Scripts/EnemyReaction.cs
+++ Assets/Scripts/EnemyReaction.cs
@@ -3,25 +3,12 @@ using UnityEngine.AI;
 
 public class EnemyReaction : MonoBehaviour
 {
-    public Renderer enemyRenderer;
-    public Color hitColor = Color.red;
-    private Color originalColor;
-    public float flashDuration = 0.1f;
-
     public NavMeshAgent agent;
     public float speedIncrease = 0.5f;
     public float maxSpeed = 6f;
 
     void Start()
     {
-        // Get renderer
-        if (enemyRenderer == null)
-            enemyRenderer = GetComponentInChildren<Renderer>();
-
-        if (enemyRenderer != null)
-            originalColor = enemyRenderer.material.color;
-
-        // Get NavMeshAgent
         if (agent == null)
             agent = GetComponent<NavMeshAgent>();
     }
@@ -30,25 +17,11 @@ public class EnemyReaction : MonoBehaviour
     {
         Debug.Log(gameObject.name + " was hit!");
 
-        // Flash red
-        if (enemyRenderer != null)
-        {
-            StopAllCoroutines();
-            StartCoroutine(FlashOnHit());
-        }
-
-        // Speed up
+        // Speed up logic
         if (agent != null)
         {
             agent.speed = Mathf.Min(agent.speed + speedIncrease, maxSpeed);
             Debug.Log("Speed increased to: " + agent.speed);
         }
-    }
-
-    private System.Collections.IEnumerator FlashOnHit()
-    {
-        enemyRenderer.material.color = hitColor;
-        yield return new WaitForSeconds(flashDuration);
-        enemyRenderer.material.color = originalColor;
     }
 }
